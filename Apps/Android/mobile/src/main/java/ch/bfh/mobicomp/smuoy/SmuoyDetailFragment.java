7bbb5f996fe74d2e51e635b33cfbe5e2b05d1626
Christian Meyer <chrigu.meyer@gmail.com>
2014-10-31 13:45:34 +0100

    Layout improvements

--- Apps/Android/mobile/src/main/java/ch/bfh/mobicomp/smuoy/SmuoyDetailFragment.java
+++ Apps/Android/mobile/src/main/java/ch/bfh/mobicomp/smuoy/SmuoyDetailFragment.java
@@ -10,6 +10,9 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import ch.bfh.mobicomp.smuoy.dummy.DummyContent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * A fragment representing a single Smuoy detail screen.
  * This fragment is either contained in a {@link SmuoyListActivity}
@@ -26,7 +29,7 @@ public class SmuoyDetailFragment extends Fragment {
     /**
      * The dummy content this fragment is presenting.
      */
-    private DummyContent.Smuoy mItem;
+    private DummyContent.Smuoy item;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -43,7 +46,7 @@ public class SmuoyDetailFragment extends Fragment {
             // Load the dummy content specified by the fragment
             // arguments. In a real-world scenario, use a Loader
             // to load content from a content provider.
-            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+            item = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
         }
     }
 
@@ -53,9 +56,9 @@ public class SmuoyDetailFragment extends Fragment {
         LinearLayout rootView = (LinearLayout) inflater.inflate(R.layout.fragment_smuoy_detail, container, false);
 
         // Show the dummy content as text in a TextView.
-        if (mItem != null) {
-            addCard(inflater, rootView, mItem.id);
-            for (DummyContent.SensorData sensor : mItem.sensorData) {
+        if (item != null) {
+            addCard(inflater, rootView, item.id);
+            for (DummyContent.SensorData sensor : item.sensorData) {
                 addCard(inflater, rootView, sensor);
             }
         }
@@ -64,25 +67,35 @@ public class SmuoyDetailFragment extends Fragment {
     }
 
     private void addCard(LayoutInflater inflater, ViewGroup parentView, CharSequence text) {
-        CardView titleCard = (CardView) inflater.inflate(R.layout.data_card_basic, parentView, false);
+        CardView card = (CardView) inflater.inflate(R.layout.data_card_basic, parentView, false);
 
-        TextView titleText = (TextView) titleCard.findViewById(R.id.textView);
-
+        TextView titleText = (TextView) card.findViewById(R.id.text);
         titleText.setText(text);
 
-        parentView.addView(titleCard);
+        parentView.addView(card);
     }
 
     private void addCard(LayoutInflater inflater, ViewGroup parentView, DummyContent.SensorData sensor) {
+        CardView card;
         if ("wind".equalsIgnoreCase(sensor.getGroup())) {
-            CardView titleCard = (CardView) inflater.inflate(R.layout.data_card_wind, parentView, false);
+            card = getCard(inflater, parentView, R.layout.data_card_wind, false);
         } else {
-            CardView titleCard = (CardView) inflater.inflate(R.layout.data_card_basic, parentView, false);
-            TextView titleText = (TextView) titleCard.findViewById(R.id.textView);
-
-            titleText.setText(sensor.toString());
-
-            parentView.addView(titleCard);
+            card = getCard(inflater, parentView, R.layout.data_card_basic, true);
+            setText(card, R.id.text, sensor.toString());
         }
+        parentView.addView(card);
+    }
+
+    private Map<Integer, CardView> cardViews = new HashMap<>();
+    private CardView getCard(LayoutInflater inflater, ViewGroup parentView, int id, boolean forceNew){
+        CardView card = cardViews.get(id);
+        if (forceNew || card == null) {
+            card = (CardView) inflater.inflate(id, parentView, false);
+        }
+        return card;
+    }
+
+    private void setText(View parent, int id, CharSequence text) {
+        ((TextView) parent.findViewById(id)).setText(text);
     }
 }
