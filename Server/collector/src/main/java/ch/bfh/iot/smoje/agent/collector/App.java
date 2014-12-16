dff17755b82ae582a250bf6e81b85ec3eeeff690
hanjo77 <hanjo77@gmail.com>
2014-12-17 00:36:48 +0100

    Updated App after DB-changes

--- Server/collector/src/main/java/ch/bfh/iot/smoje/agent/collector/App.java
+++ Server/collector/src/main/java/ch/bfh/iot/smoje/agent/collector/App.java
@@ -1,10 +1,13 @@
 package ch.bfh.iot.smoje.agent.collector;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.List;
+import ch.bfh.iot.smoje.agent.model.Measurement;
+import ch.bfh.iot.smoje.agent.model.Sensor;
+import ch.bfh.iot.smoje.agent.model.Sensorstation;
+import ch.bfh.iot.smoje.agent.model.Station;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.codec.binary.Base64;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
@@ -12,17 +15,11 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-
-import org.apache.commons.codec.binary.Base64;
-
-import ch.bfh.iot.smoje.agent.model.Measurement;
-import ch.bfh.iot.smoje.agent.model.Sensor;
-import ch.bfh.iot.smoje.agent.model.Sensorstation;
-import ch.bfh.iot.smoje.agent.model.Station;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Agent 007
@@ -36,9 +33,10 @@ public class App {
         List<Station> stations = em.createQuery("SELECT s FROM Station s").getResultList();
 
         for (Station station : stations) {
-            List<Sensorstation> sensorstations = em.createQuery("SELECT s FROM Sensorstation s").getResultList();
+            List<Sensorstation> sensorStations = em.createQuery("SELECT s FROM Sensorstation s WHERE s.station = ?1")
+		            .setParameter(1, station).getResultList();
 
-            for (Sensorstation sensorStation : sensorstations) {
+            for (Sensorstation sensorStation : sensorStations) {
                 // todo check if request is necessary
 
                 // Measurement lastMeasurement = (Measurement)
@@ -56,11 +54,12 @@ public class App {
                 // System.out.println(read);
                 //
                 if (true) {
-                    int sensorType = sensorStation.getSensor().getId();
+	                Sensor sensor = sensorStation.getSensor();
+                    int sensorType = sensor.getId();
                     switch (sensorType) {
                     case 1: // camera
                         try {
-                            writePhoto(em, station, sensorStation);
+                            writePhoto(em, station, sensor);
                         } catch (JsonProcessingException e1) {
                             // TODO Auto-generated catch block
                             e1.printStackTrace();
@@ -75,7 +74,7 @@ public class App {
                         break;
 
                     default:
-                        writeSensor(em, station, sensorStation);
+                        writeSensor(em, station, sensor);
                         break;
                     }
                 }
@@ -116,10 +115,10 @@ public class App {
         }
     }
 
-    private static void writeSensor(EntityManager em, Station station, Sensorstation sensorStation) {
+    private static void writeSensor(EntityManager em, Station station, Sensor sensor) {
 
         Client client = ClientBuilder.newBuilder().build();
-        WebTarget target = client.target(station.getUrlSensor() + sensorStation.getSensor().getName());
+        WebTarget target = client.target(station.getUrlSensor() + sensor.getName());
         String res = target.request(MediaType.APPLICATION_JSON).get(String.class);
         ObjectMapper mapper = new ObjectMapper();
 
@@ -132,6 +131,7 @@ public class App {
             // todo alert if unit is no the same as in
             // json.get("unit").asText());
             measurement.setStation(station);
+	        measurement.setSensor(sensor);
 
             em.getTransaction().begin();
             em.persist(measurement);
@@ -146,12 +146,12 @@ public class App {
         }
     }
 
-    private static void writePhoto(EntityManager em, Station station, Sensorstation sensorStation) throws JsonProcessingException, IOException {
+    private static void writePhoto(EntityManager em, Station station, Sensor sensor) throws JsonProcessingException, IOException {
 
         ObjectMapper mapper = new ObjectMapper();
 
         Client client = ClientBuilder.newBuilder().build();
-        WebTarget target = client.target(station.getUrlSensor() + sensorStation.getSensor().getName());
+        WebTarget target = client.target(station.getUrlSensor() + sensor.getName());
 
         System.out.println(target.getUri());
 
